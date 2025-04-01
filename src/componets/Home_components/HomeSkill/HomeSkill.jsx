c56e1e20971018f17e93ca4a1b11bdfdec62450b
rexton-george <rextongeorgecseb@gmail.com>
2025-04-01 09:40:22 +0530

    updated on 28/3/25

--- src/componets/Home_components/HomeSkill/HomeSkill.jsx
+++ src/componets/Home_components/HomeSkill/HomeSkill.jsx
@@ -4,6 +4,80 @@ const HomeSkill = () => {
     return(
         <div className={Style.HomeSkill}>
             <h1>Skills:</h1>
+            <div>
+                <div className={Style.cardContainer}>
+                    <div className={Style.card}>
+                        <div className="cardfsd">
+                            <div className="cardfsdin"></div>
+                            <div className="cardfsdin"></div>
+                            <div className="cardfsdin"></div>
+                        </div>
+                        <div className="cardfsd">
+                            <div className="cardfsdin"></div>
+                            <div className="cardfsdinmain"></div>
+                            <div className="cardfsdin"></div>
+                        </div>
+                        <div className="cardfsd">
+                            <div className="cardfsdin"></div>
+                            <div className="cardfsdin"></div>
+                            <div className="cardfsdin"></div>
+                        </div>
+                    </div>
+                    <div className={Style.card}>
+                        <div className="cardnw">
+                            <div className="cardnwiin"></div>
+                            <div className="cardnwiin"></div>
+                            <div className="cardnwiin"></div>
+                        </div>
+                        <div className="cardnw">
+                            <div className="cardnwiin"></div>
+                            <div className="cardnwmain"></div>
+                            <div className="cardnwiin"></div>
+                        </div>
+                        <div className="cardnw">
+                            <div className="cardnwiin"></div>
+                            <div className="cardnwiin"></div>
+                            <div className="cardnwiin"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={Style.cardContainer}>
+                    <div className={Style.card}>
+                        <div className="cardds">
+                            <div className="carddsin"></div>
+                            <div className="carddsin"></div>
+                            <div className="carddsin"></div>
+                        </div>
+                        <div className="cardds">
+                            <div className="carddsin"></div>
+                            <div className="carddsmain"></div>
+                            <div className="carddsin"></div>
+                        </div>
+                        <div className="cardds">
+                            <div className="carddsin"></div>
+                            <div className="carddsin"></div>
+                            <div className="carddsin"></div>
+                        </div>
+                    </div>
+                    <div className={Style.card}>
+                        <div className="cardai">
+                            <div className="cardaiin"></div>
+                            <div className="cardaiin"></div>
+                            <div className="cardaiin"></div>
+                        </div>
+                        <div className="cardai">
+                            <div className="cardaiin"></div>
+                            <div className="cardaimain"></div>
+                            <div className="cardaiin"></div>
+                        </div>
+                        <div className="cardai">
+                            <div className="cardaiin"></div>
+                            <div className="cardaiin"></div>
+                            <div className="cardaiin"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
